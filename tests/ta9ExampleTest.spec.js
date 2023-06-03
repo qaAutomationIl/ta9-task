3b965bc12f4d49c7e10cce851449a76ebabf0813
qaAutomationIl <judahhachiti@gmail.com>
2023-06-03 23:10:37 +0300

    adding hook

--- tests/ta9ExampleTest.spec.js
+++ tests/ta9ExampleTest.spec.js
@@ -1,19 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async({page}) => {
   await page.goto('https://xd.adobe.com/view/0b1e0e68-be9f-4b48-96fa-2a4712032331-5e4d/grid/');
   await expect(page).toHaveTitle('Flow 1');
-});
+})
 
 test('Verify homePage', async ({ page }) => {
-  const segfam1 = page.locator('Linked Screens for screen Main - list mode');
-  const segfam2 = page.locator('Linked Screens for screen Main - list mode – create new - empty');
-  const segfam3 = page.locator('Linked Screens for screen Tile mode');
-  const segfam4 = page.locator('Linked Screens for screen Main - list mode – create new – fill');
-  await expect(segfam1).toBeEnabled;
-  await expect(segfam2).toBeEnabled;
-  await expect(segfam3).toBeEnabled;
-  await expect(segfam4).toBeEnabled;
+  await expect(page.locator('Linked Screens for screen Main - list mode')).toBeVisible;
+  await expect(page.getByText('4 Screens')).toBeVisible();
 });
 
 test('Create a comment', async ({ page }) => {
